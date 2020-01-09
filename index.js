cf400c4b9924947df4088ca183083e257207a6d3
Doug Kintop <dkintop@hotmail.com>
2020-01-09 13:47:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 const fi = (function() {
   return {
     libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+      return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0";
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+      }
     },
 
-    map: function() {
+    map: function() {},
 
-    },
+    reduce: function() {},
 
-    reduce: function() {
+    functions: function() {}
+  };
+})();
 
-    },
-
-    functions: function() {
-
-    },
-
-
-  }
-})()
-
-fi.libraryMethod()
+fi.libraryMethod();
